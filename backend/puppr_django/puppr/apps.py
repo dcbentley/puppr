f15b9bb83941942b2858a584abe4e2d9123674b2
dcbentley <danielcbentley@gmail.com>
2020-09-08 14:40:56 -0400

    removing initial migration and starting over

---
+++ backend/puppr_django/puppr/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PupprConfig(AppConfig):
+    name = 'puppr'
---
+++ backend/puppr_django/puppr/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(('email'), null=True, blank=True, max_length=254)
+    pup_name = models.CharField(max_length=100)
+    profile_img_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class UserPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='User')
+    post = models.CharField(max_length=280)
+
+    def __str__(self):
+        return self.post
