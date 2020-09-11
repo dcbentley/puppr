6f280082b9eb2b12b48111a5e88aa416d1e201d9
dcbentley <danielcbentley@gmail.com>
2020-09-11 12:03:29 -0400

    added custom user

--- backend/puppr_django/puppr/admin.py
+++ backend/puppr_django/puppr/admin.py
@@ -1,4 +1,4 @@
-from import_export.damin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from .models import User, Post
 # Register your models here.
--- backend/puppr_django/puppr_django/settings.py
+++ backend/puppr_django/puppr_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,8 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dj_database_url',
     'rest_framework',
     'import_export',
+    'users',
     'puppr',
 ]
 
@@ -146,3 +149,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'puppr/build/static'),
 ]
+
+AUTH_USER_MODEL = 'users.User'
